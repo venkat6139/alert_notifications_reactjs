9b0bddd7e84521eabe19e614ca040727fd68ba9d
venkat6139 <venkatmiddey57@gmail.com>
2024-05-10 08:49:03 +0000

    committed

---
+++ src/components/Notification/index.js
@@ -0,0 +1,13 @@
+import {GrFormClose} from 'react-icons/gr'
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+  return (
+    <div className="notification-container">
+      <div className="notification-message">{children}</div>
+      <GrFormClose className="close-icon" />
+    </div>
+  )
+}
+export default Notification
